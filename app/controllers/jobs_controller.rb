e63e14d3466bff7690d776c002e29f65d7163a2e
juan.alencar <juan.alencar@vagas.com.br>
2016-09-01 21:49:53 -0300

    Usuário edita vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,12 +1,12 @@
 class JobsController < ApplicationController
+  before_action :set_job, only: [:show,:edit,:update]
+  before_action :set_companies, only: [:new,:create,:edit,:update]
 
   def show
-    @job = Job.find(params[:id])
   end
 
   def new
     @job = Job.new
-    @companies = Company.all
   end
 
   def create
@@ -15,15 +15,33 @@ class JobsController < ApplicationController
       redirect_to @job
     else
       flash[:notice] = "Não foi possível criar a vaga"
-      @companies = Company.all
       render 'new'
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      flash[:notice] = 'Não foi possível atualizar a vaga'
+      render 'edit'
+    end
+  end
 
   private
   def job_params
     params.require(:job).permit(:title,:location,:category,:company_id,:description, :featured)
+  end
+
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_companies
+    @companies = Company.all
   end
 
 end
