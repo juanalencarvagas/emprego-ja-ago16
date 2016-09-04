b1936a6616c311648f009ad58dbaf155065a03d4
Juan Alencar <juan.alencar@vagas.com.br>
2016-09-03 21:46:01 -0300

    Usuário cadastra empresas

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,32 @@
+class CompaniesController < ApplicationController
+
+
+  def new
+    @company = Company.new
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+
+  def create
+    @company = Company.new(companies_param)
+    if @company.save
+      redirect_to @company
+    else
+      flash[:notice] = "Não foi possível criar a empresa"
+      render 'new'
+    end
+
+  end
+
+  private
+  def companies_param
+    params.require(:company).permit(:name,:location,:mail,:phone)
+  end
+
+  def set_companies
+  end
+
+end
