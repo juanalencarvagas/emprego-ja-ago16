8335767a1dc30aa8226ce57626b15f4ee3668f82
juan.alencar <juan.alencar@vagas.com.br>
2016-09-05 21:53:38 -0300

    Usuário cadastra categorias

---
+++ spec/features/user_create_categories_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User create categories' do
+  scenario 'successfully' do
+
+    category = Category.new(name: 'Categoria1')
+
+    visit new_category_path
+
+    fill_in 'Nome',   with: Category.name
+
+    click_on 'Criar Categoria'
+
+    expect(page).to have_css('h1',text: category.name)
+    expect(page).to have_content(category.name)
+
+  end
+end
