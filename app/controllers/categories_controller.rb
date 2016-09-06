2a8a5ea50883c99e869f1b87982a440437a6f100
juan.alencar <juan.alencar@vagas.com.br>
2016-09-06 19:39:23 -0300

    Usuário cadastra categorias

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,24 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
   end
+
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(list_param)
+    if @category.save
+      redirect_to @category
+    else
+      render new
+    end
+  end
+
+
+  private
+  def list_param
+    params.require(:category).permit(:name)
+  end
+
 end
--- spec/features/user_create_categories_spec.rb
+++ spec/features/user_create_categories_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 feature 'User create categories' do
+
   scenario 'successfully' do
-
     category = Category.new(name: 'Categoria1')
-
     visit new_category_path
-
-    fill_in 'Nome',   with: Category.name
-
+    fill_in 'Nome',   with: category.name
     click_on 'Criar Categoria'
-
     expect(page).to have_css('h1',text: category.name)
     expect(page).to have_content(category.name)
 
   end
+
+
+
 end
